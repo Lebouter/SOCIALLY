66684188bbcc87c1401a92a53dbad999cb802f46
Nicolas <nblanchard@et.esiea.fr>
2025-03-03 20:01:51 +0100

    db sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
       <h1>home page content</h1>
